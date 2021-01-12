d128a2e7aea0b0022dff6ee7b96e008693c6068d
Matthew C.O <matconol@epsg.upv.es>
2021-01-12 23:18:26 +0100

    Comments added

--- cam.py
+++ cam.py
@@ -9,35 +9,40 @@ Author: Matthew Conde Oltra
 import cv2
 import mediapipe as mp
 
-
-
-
 class VideoCamera(object):
     
+    #Initialize important variables
     def __init__(self):
+        #Video capture 
         self.video = cv2.VideoCapture(0)
+        #Drawing utils for hands
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_hands = mp.solutions.hands
-        # For webcam input:
+        # For webcam input - initialize MediaPipe Hands
         self.hands = self.mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-
+    #
     def __del__(self):
+
         self.video.release()        
 
     def get_frame(self):
 
+        # Read each frame 
         ret, frame = self.video.read()
-        
+        # Flip the image horizontally for a later selfie-view display, and convert
+        # the BGR image to RGB.
         image = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB) 
-        
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
         image.flags.writeable = False
         results = self.hands.process(image)
 
+        # Draw hand landmarks of each hand
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
         if results.multi_hand_landmarks:
           for hand_landmarks in results.multi_hand_landmarks:
+            # Draw index finger tip coordinates
             self.mp_drawing.draw_landmarks(
                 image, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
 
